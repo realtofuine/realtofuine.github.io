26e6a0575bd2f85ad01cc91bd65abd3ec781c5eb
realtofuine <73127480+realtofuine@users.noreply.github.com>
2023-08-12 10:08:25 -0500

    fixed dark-light mode

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-window
-  .matchMedia("(prefers-color-scheme: light)")
-  .addEventListener("change", ({ matches }) => {
-    $("body").toggleClass("light-theme");
-    console.log("changed");
-  });
+// window
+//   .matchMedia("(prefers-color-scheme: light)")
+//   .addEventListener("change", ({ matches }) => {
+//     $("body").toggleClass("light-theme");
+//     console.log("changed");
+//   });
 
-// $(".theme-switch").on("click", () => {
-//   $("body").toggleClass("light-theme");
-// });
+// // $(".theme-switch").on("click", () => {
+// //   $("body").toggleClass("light-theme");
+// // });
