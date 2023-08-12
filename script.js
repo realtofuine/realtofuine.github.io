df6d4e1d5b8964678332a74919c6c4b52ecbe64a
realtofuine <73127480+realtofuine@users.noreply.github.com>
2023-08-11 23:51:08 -0500

    coolness added

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-$(".theme-switch").on("click", () => {
-  $("body").toggleClass("light-theme");
-});
+window
+  .matchMedia("(prefers-color-scheme: light)")
+  .addEventListener("change", ({ matches }) => {
+    $("body").toggleClass("light-theme");
+    console.log("changed");
+  });
+
+// $(".theme-switch").on("click", () => {
+//   $("body").toggleClass("light-theme");
+// });
